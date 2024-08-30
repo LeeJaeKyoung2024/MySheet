57f546a2a0482905262aa0698b8050cfca6a9fa2
2jkyoung2 <2jkyoung2@gmail.com>
2024-08-30 14:43:38 +0900

    first commit

---
+++ src/main/java/com/ljk/mysheet/MySheetApplication.java
@@ -0,0 +1,13 @@
+package com.ljk.mysheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySheetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljk/mysheet/MySheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljk.mysheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
